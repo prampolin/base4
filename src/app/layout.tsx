1b0ea3729de39bcf8e6a71ee29fd9182915d6e6b
prampolin <72214386+vitoradekz@users.noreply.github.com>
2023-07-18 17:18:20 -0300

    react-query setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Providers from '@/utils/provider'
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Providers>
+        <body className={inter.className}>{children}</body>
+      </Providers>
     </html>
   )
 }
